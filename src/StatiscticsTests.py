e65e28648e2715da03bdeb00f13494e5500c4943
mykolatyniv <ml546@njit.edu>
2019-10-30 17:23:23 -0400

    Fix: fixed statisticstest file

--- src/StatiscticsTests.py
+++ src/StatiscticsTests.py
@@ -36,91 +36,91 @@ class MyTestCase(unittest.TestCase):
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.population(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_variance_of_population_proportion(self):
         test_data = CsvReader('/src/test2.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.variance(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_variance_of_population_proportion(self):
         test_data = CsvReader('/src/test3.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.variance(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_z_score(self):
         test_data = CsvReader('/src/test4.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.score(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_standardized_score(self):
         test_data = CsvReader('/src/test5.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.standardized(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_population_correlation_coefficient(self):
         test_data = CsvReader('/src/test6.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.population(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_confidence_interval(self):
         test_data = CsvReader('/src/test.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.confidence(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_population_variance(self):
         test_data = CsvReader('/src/test7.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.variance(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_p_value(self):
         test_data = CsvReader('/src/test8.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.value(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_proportion(self):
         test_data = CsvReader('/src/test9.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.prop(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_sample_mean(self):
         test_data = CsvReader('/src/test10.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.smean(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_sample_standard_deviation(self):
         test_data = CsvReader('/src/test11.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.sample(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_variance_of_sample_proportion(self):
         test_data = CsvReader('/src/test12.csv').data
         pprint(test_data)
         for row in test_data:
            self.assertEqual(self.calculator.variance(row['Value 1'], row['Value 2']), int(row['Result']))
-           self.assertEqual(self.calculator.result, int(row['Result'])
+           self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
