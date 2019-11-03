9a1efec8321967c7321f931c61acd777ce7fa6a9
shellynj <mb884@njit.edu>
2019-11-03 10:39:58 -0500

    Feature: Added Definition of Correlation Coefficient with check for invalid input. Invalid input throws an exception.

--- src/Statistics.py
+++ src/Statistics.py
@@ -63,9 +63,59 @@ def standardized_score(a, b):
     c= a + b
     return c
 
-def correlation_coefficient(a, b):
-    c= a + b
-    return c
+#def correlation_coefficient(a, b):
+#    c= a + b
+#    return c
+
+##TEMPORARY TESTING with PROVIDED VALUES
+def correlationCoefficient(a, b, n) :
+    try:
+        sum_a = 0
+        sum_b = 0
+        sum_ab = 0
+        squareSum_a = 0
+        squareSum_b = 0
+        i = 0
+        while i < n:
+            # sum of elements of array X.
+            sum_a = sum_a + a[i]
+
+            # sum of elements of array Y.
+            sum_b = sum_b + b[i]
+
+            # sum of X[i] * Y[i].
+            sum_ab = sum_ab + a[i] * b[i]
+
+            # sum of square of array elements.
+            squareSum_a = squareSum_a + a[i] * a[i]
+            squareSum_b = squareSum_b + b[i] * b[i]
+
+            i = i + 1
+
+        # Actual Formula for Correlation Coefficient
+        # corr = (float(n * sum_ab - sum_a * sum_b)) /(float(math.sqrt((n * squareSum_a - sum_a * sum_b) * (n * squareSum_b - sum_b * sum_b))))
+        # Breakdown of formula for Correlation Coefficient.
+        c = float(n * squareSum_a - sum_a * sum_a)
+        d = float(n * squareSum_b - sum_b * sum_b)
+        e = float(math.sqrt(c * d))
+        f = float(n * sum_ab - sum_a * sum_b)
+        corr = float(f / e)
+        return corr
+    except ZeroDivisionError:
+        print("Error: Dividing by Zero is not valid!!")
+    except ValueError:
+        print ("Error: Only Numeric Values are valid!!")
+
+a = [15, 18, 21, 24, 27]
+b = [25, 25, 27, 31, 32]
+
+    # Find the size of array.
+n = len(b)
+print(correlationCoefficient(a, b, n))
+
+
+
+
 
 def confidence_interval(a, b):
     c= a + b
