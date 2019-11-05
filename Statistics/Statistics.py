571b9030ef84ff8e54c61fc4fd6b448908731c7d
mykolatyniv <ml546@njit.edu>
2019-11-04 21:38:53 -0500

    Fix: fixed stat file

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,3 +1,19 @@
+from Statistics.population_mean import population_mean
+from Statistics.median import median
+from Statistics.mode import mode
+from Statistics.population_standard_deviation import population_standard_deviation
+from Statistics.variance_of_population_proportion import variance_of_population_proportion
+from Statistics.z_score import z_score
+from Statistics.standardized_score import standardized_score
+from Statistics.correlation_coefficient import correlation_coefficient
+from Statistics.confidence_interval import confidence_interval
+from Statistics.population_variance import population_variance
+from Statistics.p_value import p_value
+from Statistics.proportion import proportion
+from Statistics.sample_mean import sample_mean
+from Statistics.sample_standard_deviation import sample_standard_deviation
+from Statistics.variance_of_sample_proportion import variance_of_sample_proportion
+
 class Statistics:
     result = 0
 
