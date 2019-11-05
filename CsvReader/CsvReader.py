6d2f200ec57757b6a6418608df911e665aef299f
mykolatyniv <ml546@njit.edu>
2019-11-05 13:39:56 -0500

    Task: removed files

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -1,5 +1,5 @@
 import csv
-from Fileutilities.absolutepath import absolutepath
+from Fileutilities.absolutepath import absolute_path
 from pprint import pprint
 
 def ClassFactory(class_name, dictionary):
@@ -11,7 +11,7 @@ class CsvReader:
     def __init__(self, filepath):
         self.data = []
 
-        with open(absolutepath(filepath)) as text_data:
+        with open(absolute_path(filepath)) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
