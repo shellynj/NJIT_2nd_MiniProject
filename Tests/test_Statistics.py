0ee311551b68af6c92f5d85bc9e072c12d395c63
mykolatyniv <ml546@njit.edu>
2019-11-06 18:24:37 -0500

    Task: updated file

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -133,5 +133,6 @@ class MyTestCase(unittest.TestCase):
     def test_results_property(self):
         self.assertEqual(self.statistics.result, 0)
 
+
 if __name__ == '__main__':
     unittest.main()
