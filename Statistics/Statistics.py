f6af3b889ae8cd5a47478a34f58061ed07c4ddc2
mykolatyniv <ml546@njit.edu>
2019-11-06 20:48:51 -0500

    Fix: fixed files for travis 11/6/2019

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -19,7 +19,7 @@ class Statistics(Calculator):
     data = []
 
     def __init__(self):
-        pass
+        super().__init__()
 
     def mean(self, a, b, c):
         self.result = mean(a, b, c)
