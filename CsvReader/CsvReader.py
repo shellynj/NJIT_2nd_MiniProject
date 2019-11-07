bfce28810cd84778755e2814192e8a89d3bd83be
mykolatyniv <ml546@njit.edu>
2019-11-06 20:24:19 -0500

    Fix: fixed files for travis

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -1,4 +1,5 @@
 import csv
+from Fileutilities.absolutepath import absolutepath
 from pprint import pprint
 
 def ClassFactory(class_name, dictionary):
@@ -9,7 +10,7 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-        with open(filepath) as text_data:
+        with open(absolutepath(filepath)) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
