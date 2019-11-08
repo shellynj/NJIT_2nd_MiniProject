a3853e05f794a242a9156e7739d19670612bc0a6
mykolatyniv <ml546@njit.edu>
2019-11-08 14:36:20 -0500

    Fix: fixed  file

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -15,6 +15,7 @@ from Statistics.Sample_mean import sample_mean
 from Statistics.sample_standard_deviation import sample_standard_deviation
 from Statistics.Variance_of_sample_proportion import variance_of_sample_proportion
 
+
 class Statistics(Calculator):
     result = 0
 
